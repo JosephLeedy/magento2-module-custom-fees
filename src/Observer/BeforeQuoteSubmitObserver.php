9fa168a215330441b65bc31b1f136b16afd7d038
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2024-10-07 17:06:51 -0600

    Copy fees from quote to order before quote submission

---
+++ src/Observer/BeforeQuoteSubmitObserver.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JosephLeedy\CustomFees\Observer;
+
+use JosephLeedy\CustomFees\Api\Data\CustomOrderFeesInterface;
+use JosephLeedy\CustomFees\Api\Data\CustomOrderFeesInterfaceFactory;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Quote\Api\Data\CartInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+
+use function method_exists;
+
+/**
+ * Observer for `sales_model_service_quote_submit_before` event
+ *
+ * @see \Magento\Quote\Model\QuoteManagement::submitQuote()
+ */
+class BeforeQuoteSubmitObserver implements ObserverInterface
+{
+    public function __construct(private readonly CustomOrderFeesInterfaceFactory $customOrderFeesFactory)
+    {}
+
+    public function execute(Observer $observer): void
+    {
+        $event = $observer->getEvent();
+        /** @var CartInterface $quote */
+        $quote = $event->getData('quote');
+        /** @var OrderInterface $order */
+        $order = $event->getData('order');
+        $quoteExtension = $quote->getExtensionAttributes();
+        $orderExtension = $order->getExtensionAttributes();
+
+        if (
+            $quoteExtension === null
+            || $orderExtension === null
+            || !method_exists($quoteExtension, 'getCustomFees')
+            || !method_exists($orderExtension, 'setCustomOrderFees')
+            || $quoteExtension->getCustomFees() === null
+        ) {
+            return;
+        }
+
+        /** @var CustomOrderFeesInterface $customOrderFees */
+        $customOrderFees = $this->customOrderFeesFactory->create();
+        /** @var array<string, array{code: string, title: string, base_value: float, value: float}> $customFees */
+        $customFees = $quoteExtension->getCustomFees();
+
+        $customOrderFees->setCustomFees($customFees);
+
+        $orderExtension->setCustomOrderFees($customOrderFees);
+    }
+}
---
+++ test/Integration/Observer/BeforeQuoteSubmitObserverTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JosephLeedy\CustomFees\Test\Integration\Observer;
+
+use JosephLeedy\CustomFees\Api\Data\CustomOrderFeesInterface;
+use JosephLeedy\CustomFees\Observer\BeforeQuoteSubmitObserver;
+use Magento\Framework\Event\ConfigInterface as EventObserverConfig;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Magento\Quote\Model\Quote;
+use Magento\Quote\Model\ResourceModel\Quote as QuoteResource;
+use Magento\Sales\Api\Data\OrderExtension;
+use Magento\Sales\Model\Order;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+
+final class BeforeQuoteSubmitObserverTest extends TestCase
+{
+    /**
+     * @magentoAppArea frontend
+     */
+    public function testIsConfiguredCorrectly(): void
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        /** @var EventObserverConfig $observerConfig */
+        $observerConfig = $objectManager->create(EventObserverConfig::class);
+        $observers = $observerConfig->getObservers('sales_model_service_quote_submit_before');
+
+        self::assertArrayHasKey('add_custom_fees_to_order', $observers);
+        self::assertSame(
+            ltrim(BeforeQuoteSubmitObserver::class, '\\'),
+            $observers['add_custom_fees_to_order']['instance']
+        );
+    }
+
+    /**
+     * @phpcs:ignore Generic.Files.LineLength.TooLong
+     * @magentoConfigFixture current_store sales/custom_order_fees/custom_fees [{"code":"test_fee_0","title":"Test Fee","value":"4.00"},{"code":"test_fee_1","title":"Another Fee","value":"1.00"}]
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_shipping_method.php
+     */
+    public function testAddsCustomFeesToOrder(): void
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        /** @var Quote $quote */
+        $quote = $objectManager->create(Quote::class);
+        /** @var QuoteResource $quoteResource */
+        $quoteResource = $objectManager->create(QuoteResource::class);
+        /** @var Order $order */
+        $order = $objectManager->create(Order::class);
+        /** @var OrderExtension $orderExtension */
+        $orderExtension = $objectManager->create(OrderExtension::class);
+        /** @var EventManager $eventManager */
+        $eventManager = $objectManager->create(EventManager::class);
+
+        $quoteResource->load($quote, 'test_order_1', 'reserved_order_id');
+
+        $quote->collectTotals();
+
+        $order->setExtensionAttributes($orderExtension);
+
+        $eventManager->dispatch(
+            'sales_model_service_quote_submit_before',
+            [
+                'quote' => $quote,
+                'order' => $order
+            ]
+        );
+
+        self::assertInstanceOf(
+            CustomOrderFeesInterface::class,
+            $order->getExtensionAttributes()?->getCustomOrderFees()
+        );
+    }
+}
