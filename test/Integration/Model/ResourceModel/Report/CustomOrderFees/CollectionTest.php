fe4b5fa6362c549133db174ce6a4e6efbd059036
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-04-30 15:51:31 -0600

    Create Collection for Custom Order Fees report

---
+++ test/Integration/Model/ResourceModel/Report/CustomOrderFees/CollectionTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JosephLeedy\CustomFees\Test\Integration\Model\ResourceModel\Report\CustomOrderFees;
+
+use JosephLeedy\CustomFees\Model\ResourceModel\Report\CustomOrderFees\Collection;
+use Magento\TestFramework\Fixture\AppArea;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+use Zend_Db_Expr;
+
+use function array_column;
+
+#[AppArea('adminhtml')]
+final class CollectionTest extends TestCase
+{
+    /**
+     * @dataProvider selectsAndGroupsCorrectFieldsDataProvider
+     * @param array<string, string> $aggregatedColumns
+     * @param array<int, string> $expectedColumns
+     * @param array<int, string|Zend_Db_Expr> $expectedGroups
+     */
+    public function testSelectsAndGroupsCorrectFields(
+        array $aggregatedColumns,
+        array $expectedColumns,
+        array $expectedGroups,
+    ): void {
+        $objectManager = Bootstrap::getObjectManager();
+        /** @var Collection $collection */
+        $collection = $objectManager->create(Collection::class);
+
+        if (count($aggregatedColumns) > 0) {
+            $collection->setAggregatedColumns($aggregatedColumns);
+            $collection->isTotals(true);
+        }
+
+        $collection->load();
+
+        $select = $collection->getSelect();
+        $actualColumns = array_column((array) $select->getPart('columns'), 2);
+        $actualGroups = $select->getPart('group');
+
+        self::assertSame($expectedColumns, $actualColumns);
+        self::assertEquals($expectedGroups, $actualGroups);
+    }
+
+    /**
+     * @return array<string, array<string, array<int|string, string|Zend_Db_Expr>>>
+     */
+    public static function selectsAndGroupsCorrectFieldsDataProvider(): array
+    {
+        return [
+            'without totals' => [
+                'aggregatedColumns' => [],
+                'expectedColumns' => [
+                    'period',
+                    'fee_title',
+                    'base_fee_amount',
+                    'paid_fee_amount',
+                    'paid_order_currency',
+                    'invoiced_fee_amount',
+                ],
+                'expectedGroups' => [
+                    new Zend_Db_Expr('DATE_FORMAT(period, \'%Y-%m-%d\')'),
+                    'fee_title',
+                    'paid_order_currency',
+                ],
+            ],
+            'with totals' => [
+                'aggregatedColumns' => [
+                    'base_fee_amount' => 'sum(base_fee_amount)',
+                    'paid_fee_amount' => 'sum(paid_fee_amount)',
+                    'paid_order_currency' => 'paid_order_currency',
+                    'invoiced_fee_amount' => 'sum(invoiced_fee_amount)',
+                ],
+                'expectedColumns' => [
+                    'base_fee_amount',
+                    'paid_fee_amount',
+                    'paid_order_currency',
+                    'invoiced_fee_amount',
+                ],
+                'expectedGroups' => [
+                    'paid_order_currency',
+                ],
+            ],
+        ];
+    }
+}
