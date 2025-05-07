64b9a8923220e96533f022b530001dff17f114cf
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-05-07 11:10:18 -0600

    Add example file link to Import Custom Fees field

--- src/Block/System/Config/Form/Field/ImportCustomFees.php
+++ src/Block/System/Config/Form/Field/ImportCustomFees.php
@@ -7,8 +7,23 @@ namespace JosephLeedy\CustomFees\Block\System\Config\Form\Field;
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
 
+use function str_replace;
+
 class ImportCustomFees extends Field
 {
+    protected function _renderValue(AbstractElement $element)
+    {
+        $element->setComment(
+            str_replace(
+                '{{exampleCsvUrl}}',
+                $this->_urlBuilder->getUrl('custom_fees/config/export_exampleCsv'),
+                (string) $element->getComment(),
+            ),
+        );
+
+        return parent::_renderValue($element);
+    }
+
     protected function _isInheritCheckboxRequired(AbstractElement $element)
     {
         return false;
---
+++ src/Controller/Adminhtml/Config/Export/ExampleCsv.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JosephLeedy\CustomFees\Controller\Adminhtml\Config\Export;
+
+use Exception;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\Response\Http\FileFactory;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Filesystem;
+
+use function microtime;
+
+class ExampleCsv implements HttpGetActionInterface
+{
+    public function __construct(private readonly Filesystem $filesystem, private readonly FileFactory $fileFactory) {}
+
+    /**
+     * @throws Exception
+     */
+    public function execute(): ResponseInterface
+    {
+        $content = [
+            'type' => 'filename',
+            'value' => $this->createTemporaryCsvFile(),
+            'rm' => true,
+        ];
+
+        return $this->fileFactory->create('custom-fees-import.csv', $content, DirectoryList::VAR_DIR);
+    }
+
+    private function createTemporaryCsvFile(): string
+    {
+        $directory = $this->filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
+
+        $directory->create('export');
+
+        $filePath = 'export/custom_fees_' . microtime() . '.csv';
+        $stream = $directory->openFile($filePath, 'w+');
+
+        $stream->lock();
+        $stream->write('code,title,value');
+        $stream->unlock();
+        $stream->close();
+
+        return $filePath;
+    }
+}
