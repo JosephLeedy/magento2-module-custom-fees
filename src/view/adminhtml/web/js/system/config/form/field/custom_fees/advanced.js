882a24f165fa23abb4632663e10a7875728c2e50
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-05-27 17:00:30 -0600

    Save selected conditions in Advanced config field

--- src/view/adminhtml/web/js/system/config/form/field/custom_fees/advanced.js
+++ src/view/adminhtml/web/js/system/config/form/field/custom_fees/advanced.js
@@ -8,6 +8,103 @@ define(
     function($, modal) {
         'use strict';
 
+        const convertFormDataToNestedObject = function (formData) {
+            const nestedObject = {};
+
+            formData.forEach(({name, value}) => {
+                // Parse the path: e.g., "rule[conditions][1][type]" -> ['rule', 'conditions', '1', 'type']
+                const path = [];
+                const regex = /([^\[\]]+)|\[(.*?)]/g;
+                let match;
+                let current = nestedObject;
+
+                while ((match = regex.exec(name))) {
+                    if (match[1]) {
+                        path.push(match[1]);
+                    } else if (match[2]) {
+                        path.push(match[2]);
+                    }
+                }
+
+                // Walk/create the nested path
+                for (let i = 0; i < path.length - 1; i++) {
+                    if (!current[path[i]]) {
+                        current[path[i]] = {};
+                    }
+
+                    current = current[path[i]];
+                }
+
+                current[path[path.length - 1]] = value;
+            });
+
+            return nestedObject;
+        };
+
+        const convertFlatDataToRecursiveData = function (flatData) {
+            const recursiveData = {};
+
+            for (const [key, value] of Object.entries(flatData)) {
+                for (const [id, data] of Object.entries(value)) {
+                    const path = id.split('--');
+                    let node = recursiveData;
+
+                    for (let i = 0, length = path.length; i < length; i++) {
+                        if (!node[key]) {
+                            node[key] = [];
+                        }
+
+                        if (!node[key][path[i]]) {
+                            node[key][path[i]] = {};
+                        }
+
+                        node = node[key][path[i]];
+                    }
+
+                    for (const [childKey, childValue] of Object.entries(data)) {
+                        if (childKey === 'new_child') {
+                            continue;
+                        }
+
+                        node[childKey] = childValue;
+                    }
+                }
+            }
+
+            return recursiveData;
+        };
+
+        const saveRuleData = function () {
+            const formData = $(this.element).find('input, select').serializeArray();
+            const $advancedInput = $('#' + $(this.element).data('input-id'));
+            let nestedFormData;
+            let rule;
+            let conditionsJson;
+
+            if (formData.length === 0 || $advancedInput.length === 0) {
+                return;
+            }
+
+            nestedFormData = convertFormDataToNestedObject(formData);
+
+            if (!nestedFormData.hasOwnProperty('rule')) {
+                return;
+            }
+
+            rule = convertFlatDataToRecursiveData(nestedFormData.rule);
+
+            if (!rule.hasOwnProperty('conditions') || rule.conditions.length === 0) {
+                return;
+            }
+
+            conditionsJson = JSON.stringify(
+                rule['conditions'][1],
+                (key, value) => key === 'conditions' ? value.filter(Boolean) : value
+            );
+
+            $advancedInput.val(`{"conditions":${conditionsJson}}`);
+        };
+
         const beforeModalClose = function () {
             $(this.element).html('');
         };
@@ -19,6 +116,7 @@ define(
         };
 
         const handleModalSave = function () {
+            saveRuleData.call(this);
             beforeModalClose.call(this);
 
             this.closeModal();
