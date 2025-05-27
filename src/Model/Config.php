fcdcf2452d18a7bbeef12bd0d6201a51fccef8e6
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-05-27 17:10:47 -0600

    Process advanced settings when processing configuration

--- src/Model/Config.php
+++ src/Model/Config.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace JosephLeedy\CustomFees\Model;
 
-use JosephLeedy\CustomFees\Api\ConfigInterface;
 use InvalidArgumentException;
+use JosephLeedy\CustomFees\Api\ConfigInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Serialize\SerializerInterface;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\StoreManagerInterface;
+
+use function array_key_exists;
+use function str_replace;
 
 class Config implements ConfigInterface
 {
@@ -39,7 +42,9 @@ class Config implements ConfigInterface
         ) ?? '[]';
 
         try {
-            /** @var array{code: string, title: string, value: float}[] $customFees */
+            /**
+             * @var array{code: string, title: string, value: float, advanced?: string}[] $customFees
+             */
             $customFees = $this->serializer->unserialize($customFeesJson);
         } catch (InvalidArgumentException $invalidArgumentException) {
             throw new LocalizedException(
@@ -47,6 +52,53 @@ class Config implements ConfigInterface
             );
         }
 
+        array_walk(
+            $customFees,
+            function (array &$customFee): void {
+                if (!array_key_exists('advanced', $customFee)) {
+                    $customFee['advanced'] = '[]';
+                }
+
+                try {
+                    $customFee['advanced'] = $this->serializer->unserialize(
+                        str_replace('\\', '\\\\', $customFee['advanced']),
+                    );
+                } catch (InvalidArgumentException $invalidArgumentException) {
+                    throw new LocalizedException(
+                        __(
+                            'Could not process advanced configuration for custom fee "%1". Error: "%2"',
+                            $customFee['code'],
+                            $invalidArgumentException->getMessage(),
+                        ),
+                        $invalidArgumentException,
+                    );
+                }
+            },
+        );
+
+        /**
+         * @var array{
+         *      code: string,
+         *      title: string,
+         *      value: float,
+         *      advanced: array{
+         *          conditions?: array{
+         *              type: class-string,
+         *              aggregator: string,
+         *              value: '0'|'1',
+         *              conditions: array<
+         *                  int,
+         *                  array{
+         *                      type: class-string,
+         *                      attribute: string,
+         *                      operator: string,
+         *                      value: string
+         *                  }
+         *              >
+         *          }
+         *      }
+         *  }[] $customFees
+         */
         return $customFees;
     }
 }
