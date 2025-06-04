f68a6652abfd9ca8d81496356210123ff9955733
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-06-04 13:25:14 -0600

    Get product from quote item during condition validation

--- src/Model/Rule/Condition/Product.php
+++ src/Model/Rule/Condition/Product.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace JosephLeedy\CustomFees\Model\Rule\Condition;
 
+use Magento\Framework\Model\AbstractModel;
+use Magento\Quote\Model\Quote\Item;
 use Magento\Rule\Model\Condition\Product\AbstractProduct;
 
 class Product extends AbstractProduct
 {
     protected $_isUsedForRuleProperty = 'is_used_for_custom_fee_rules';
+
+    public function validate(AbstractModel $model): bool
+    {
+        if (!$model instanceof Item) {
+            return true;
+        }
+
+        return parent::validate($model->getProduct());
+    }
 }
