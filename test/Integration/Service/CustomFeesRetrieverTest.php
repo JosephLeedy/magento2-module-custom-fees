d9f379204e55f9b0ce505e286971ad31f766a046
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-06-18 11:38:11 -0600

    Add type field to Custom Fees Retriever Test

--- test/Integration/Service/CustomFeesRetrieverTest.php
+++ test/Integration/Service/CustomFeesRetrieverTest.php
@@ -54,12 +54,14 @@ final class CustomFeesRetrieverTest extends TestCase
             '_1727299833817_817' => [
                 'code' => 'test_fee_0',
                 'title' => 'Test Fee',
+                'type' => 'fixed',
                 'base_value' => 5.00,
                 'value' => 5.00,
             ],
             '_1727299843197_197' => [
                 'code' => 'test_fee_1',
                 'title' => 'Another Test Fee',
+                'type' => 'fixed',
                 'base_value' => 1.50,
                 'value' => 1.50,
             ],
