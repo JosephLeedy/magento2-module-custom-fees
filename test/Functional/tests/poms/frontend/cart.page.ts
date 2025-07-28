ec44011c50a36d7cb3133892c6af2389a5cd8b15
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-07-28 16:20:27 -0600

    Refactor custom fee retrieval logic to separate class
    Allows logic to be reused without requiring duplication.

--- test/Functional/tests/poms/frontend/cart.page.ts
+++ test/Functional/tests/poms/frontend/cart.page.ts
@@ -1,5 +1,6 @@
 import { expect, Locator } from '@playwright/test';
-import { inputValuesCustomFees, UIReferenceCustomFees } from '@config';
+import { UIReferenceCustomFees } from '@config';
+import CustomFees from '@utils/customFees.utils';
 import BaseCartPage from 'base-tests/poms/frontend/cart.page';
 
 class CartPage extends BaseCartPage
@@ -24,7 +25,8 @@ class CartPage extends BaseCartPage
 
     public async hasCustomFees(inEuro: boolean = false, exclude: string[] = []): Promise<void>
     {
-        const customFees = await this.getCustomFees(inEuro, exclude);
+        const cartSummaryLocator = this.page.locator(UIReferenceCustomFees.cartPage.cartSummaryLocator);
+        const customFees = await new CustomFees().getAll(cartSummaryLocator, inEuro, exclude);
 
         for (const customFee of customFees) {
             await expect(customFee).toBeVisible();
@@ -33,57 +35,12 @@ class CartPage extends BaseCartPage
 
     public async doesNotHaveCustomFees(inEuro: boolean = false, exclude: string[] = []): Promise<void>
     {
-        const customFees = await this.getCustomFees(inEuro, exclude);
+        const cartSummaryLocator = this.page.locator(UIReferenceCustomFees.cartPage.cartSummaryLocator);
+        const customFees = await new CustomFees().getAll(cartSummaryLocator, inEuro, exclude);
 
         for (const customFee of customFees) {
             await expect(customFee).not.toBeVisible();
         }
-    }
-
-    private async getCustomFees(inEuro: boolean = false, exclude: string[] = []): Promise<Locator[]>
-    {
-        const customFees = [];
-        const cartSummary = this.page.locator(UIReferenceCustomFees.cartPage.cartSummaryLocator);
-        const currencySymbol = inEuro ? '€' : '$';
-        /* The regex below is naïve in that it does not account for the currency format, but it's not necessary to
-           do so right now. If it becomes necessary, we'll look into improving this logic. */
-        const subtotal = parseFloat(
-            (
-                await cartSummary
-                    .getByText(`${UIReferenceCustomFees.cartPage.subtotalTotalLabel} ${currencySymbol}`)
-                    .textContent()
-                ?? '0'
-            ).replace(/[^\d.]+/, '')
-        );
-
-        for (const feeName in inputValuesCustomFees.customFees) {
-            let label = inputValuesCustomFees.customFees[feeName].title;
-            let amount = !inEuro
-                ? inputValuesCustomFees.customFees[feeName].base_amount
-                : inputValuesCustomFees.customFees[feeName].amount;
-
-            if (exclude.includes(feeName)) {
-                continue;
-            }
-
-            if (
-                inputValuesCustomFees.customFees[feeName].hasOwnProperty('advanced')
-                && inputValuesCustomFees.customFees[feeName].advanced.hasOwnProperty('show_percentage')
-                && inputValuesCustomFees.customFees[feeName].advanced.show_percentage
-            ) {
-                label += ` (${inputValuesCustomFees.customFees[feeName].amount}%)`;
-            }
-
-            if (inputValuesCustomFees.customFees[feeName].type.toLowerCase() === 'percent') {
-                amount = ((amount * subtotal) / 100).toFixed(2);
-            }
-
-            label += ` ${currencySymbol}${amount}`;
-
-            customFees.push(cartSummary.getByText(label, { exact: true }));
-        }
-
-        return customFees;
     }
 }
 
---
+++ test/Functional/tests/utils/customFees.utils.ts
@@ -0,0 +1,48 @@
+import { Locator } from '@playwright/test';
+import { inputValuesCustomFees, UIReferenceCustomFees } from '@config';
+
+class CustomFees
+{
+    public async getAll(containerLocator: Locator, inEuro: boolean = false, exclude: string[] = []): Promise<Locator[]>
+    {
+        const customFees = [];
+        const currencySymbol = inEuro ? '€' : '$';
+        const subtotal = parseFloat(
+            /* The regex below is naïve in that it does not account for the currency format, but it's not necessary to
+               do so right now. If it becomes necessary, we'll look into improving this logic. */
+            (await containerLocator.getByText(`Subtotal ${currencySymbol}`).textContent() ?? '0').replace(/[^\d.]+/, '')
+        );
+        let feeName;
+
+        for (feeName in inputValuesCustomFees.customFees) {
+            let label = inputValuesCustomFees.customFees[feeName].title;
+            let amount = !inEuro
+                ? inputValuesCustomFees.customFees[feeName].base_amount
+                : inputValuesCustomFees.customFees[feeName].amount;
+
+            if (exclude.includes(feeName)) {
+                continue;
+            }
+
+            if (
+                inputValuesCustomFees.customFees[feeName].hasOwnProperty('advanced')
+                && inputValuesCustomFees.customFees[feeName].advanced.hasOwnProperty('show_percentage')
+                && inputValuesCustomFees.customFees[feeName].advanced.show_percentage
+            ) {
+                label += ` (${inputValuesCustomFees.customFees[feeName].amount}%)`;
+            }
+
+            if (inputValuesCustomFees.customFees[feeName].type.toLowerCase() === 'percent') {
+                amount = ((amount * subtotal) / 100).toFixed(2);
+            }
+
+            label += ` ${currencySymbol}${amount}`;
+
+            customFees.push(containerLocator.getByText(label, { exact: true }));
+        }
+
+        return customFees;
+    }
+}
+
+export default CustomFees;
