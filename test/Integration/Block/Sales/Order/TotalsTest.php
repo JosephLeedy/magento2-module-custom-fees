70b5aa2e8dc73c5e44872d3e7d7fe4db258a8fe9
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2025-10-31 17:15:53 -0600

    Initialize sales order fee totals by display type

--- test/Integration/Block/Sales/Order/TotalsTest.php
+++ test/Integration/Block/Sales/Order/TotalsTest.php
@@ -4,20 +4,29 @@ declare(strict_types=1);
 
 namespace JosephLeedy\CustomFees\Test\Integration\Block\Sales\Order;
 
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+use JosephLeedy\CustomFees\Api\ConfigInterface;
 use JosephLeedy\CustomFees\Block\Sales\Order\Totals as CustomOrderFeesTotalsBlock;
+use JosephLeedy\CustomFees\Model\DisplayType;
 use JosephLeedy\CustomFees\Service\CustomFeesRetriever;
+use Magento\Framework\DataObject;
 use Magento\Framework\DataObjectFactory;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Sales\Block\Order\Totals as OrderTotalsBlock;
 use Magento\Sales\Model\Order;
-use Magento\TestFramework\Annotation\DataFixture;
+use Magento\TestFramework\Annotation\DataFixture as DataFixtureAnnotation;
+use Magento\TestFramework\Fixture\DataFixture as DataFixtureAttribute;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\Workaround\Override\Fixture\Resolver;
 use PHPUnit\Framework\TestCase;
 
+use function __;
+
 final class TotalsTest extends TestCase
 {
+    use ArraySubsetAsserts;
+
     /**
      * @dataProvider initTotalsDataProvider
      * @param 'does'|'does not' $condition
@@ -34,7 +43,7 @@ final class TotalsTest extends TestCase
 
         $resolver = Resolver::getInstance();
 
-        $resolver->setCurrentFixtureType(DataFixture::ANNOTATION);
+        $resolver->setCurrentFixtureType(DataFixtureAnnotation::ANNOTATION);
         $resolver->requireDataFixture("JosephLeedy_CustomFees::../test/Integration/_files/$filename.php");
 
         /** @var ObjectManagerInterface $objectManager */
@@ -49,6 +58,7 @@ final class TotalsTest extends TestCase
                     'context' => $objectManager->get(Context::class),
                     'customFeesRetriever' => $objectManager->create(CustomFeesRetriever::class),
                     'dataObjectFactory' => $objectManager->get(DataObjectFactory::class),
+                    'config' => $objectManager->create(ConfigInterface::class),
                     'data' => [],
                 ],
             )->onlyMethods(
@@ -74,6 +84,117 @@ final class TotalsTest extends TestCase
     }
 
     /**
+     * @dataProvider initializesTotalsByDisplayTypeDataProvider
+     */
+    #[DataFixtureAttribute('JosephLeedy_CustomFees::../test/Integration/_files/order_with_custom_fees_taxed.php')]
+    public function testInitializesTotalsByDisplayType(DisplayType $displayType): void
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        /** @var Order $order */
+        $order = $objectManager->create(Order::class);
+        $configStub = $this->createStub(ConfigInterface::class);
+        /** @var OrderTotalsBlock $orderTotalsBlock */
+        $orderTotalsBlock = $objectManager->create(OrderTotalsBlock::class);
+        $customOrderFeesTotalsBlock = $this
+            ->getMockBuilder(CustomOrderFeesTotalsBlock::class)
+            ->setConstructorArgs(
+                [
+                    'context' => $objectManager->get(Context::class),
+                    'customFeesRetriever' => $objectManager->create(CustomFeesRetriever::class),
+                    'dataObjectFactory' => $objectManager->get(DataObjectFactory::class),
+                    'config' => $configStub,
+                    'data' => [],
+                ],
+            )->onlyMethods(
+                [
+                    'getParentBlock',
+                ],
+            )->getMock();
+
+        $order->loadByIncrementId('100000001');
+
+        $configStub
+            ->method('getSalesDisplayType')
+            ->willReturn($displayType);
+
+        $customOrderFeesTotalsBlock
+            ->method('getParentBlock')
+            ->willReturn($orderTotalsBlock);
+
+        $orderTotalsBlock->setOrder($order);
+        $orderTotalsBlock->toHtml();
+
+        $customOrderFeesTotalsBlock->initTotals();
+
+        $expectedOrderTotals = [
+            'test_fee_0' => $objectManager->create(
+                DataObject::class,
+                [
+                    'data' => [
+                        'code' => 'test_fee_0',
+                        'label' => __('Test Fee'),
+                        'base_value' => 5.42,
+                        'value' => 5.42,
+                    ],
+                ],
+            ),
+            'test_fee_1' => $objectManager->create(
+                DataObject::class,
+                [
+                    'data' => [
+                        'code' => 'test_fee_1',
+                        'label' => __('Another Test Fee'),
+                        'base_value' => 1.63,
+                        'value' => 1.63,
+                    ],
+                ],
+            ),
+        ];
+
+        if ($displayType === DisplayType::Both) {
+            $expectedOrderTotals['test_fee_0']->setData('label', __('%1 Excl. Tax', __('Test Fee')));
+            $expectedOrderTotals['test_fee_0']->setData('base_value', 5.00);
+            $expectedOrderTotals['test_fee_0']->setData('value', 5.00);
+
+            $expectedOrderTotals['test_fee_1']->setData('label', __('%1 Excl. Tax', __('Another Test Fee')));
+            $expectedOrderTotals['test_fee_1']->setData('base_value', 1.50);
+            $expectedOrderTotals['test_fee_1']->setData('value', 1.50);
+
+            $expectedOrderTotals = array_slice(array: $expectedOrderTotals, offset: 0, length: 1, preserve_keys: true)
+                + [
+                    'test_fee_0_with_tax' => $objectManager->create(
+                        DataObject::class,
+                        [
+                            'data' => [
+                                'code' => 'test_fee_0_with_tax',
+                                'label' => __('%1 Incl. Tax', __('Test Fee')),
+                                'base_value' => 5.42,
+                                'value' => 5.42,
+                            ],
+                        ],
+                    ),
+                ] + array_slice(array: $expectedOrderTotals, offset: 1, preserve_keys: true);
+            $expectedOrderTotals += [
+                'test_fee_1_with_tax' => $objectManager->create(
+                    DataObject::class,
+                    [
+                        'data' => [
+                            'code' => 'test_fee_1_with_tax',
+                            'label' => __('%1 Incl. Tax', __('Another Test Fee')),
+                            'base_value' => 1.63,
+                            'value' => 1.63,
+                        ],
+                    ],
+                ),
+            ];
+        }
+
+        $actualOrderTotals = $orderTotalsBlock->getTotals();
+
+        self::assertArraySubset($expectedOrderTotals, $actualOrderTotals);
+    }
+
+    /**
      * @return string[][]
      */
     public function initTotalsDataProvider(): array
@@ -84,6 +205,21 @@ final class TotalsTest extends TestCase
             ],
             'does not initialize totals for order without custom fees' => [
                 'condition' => 'does not',
+            ],
+        ];
+    }
+
+    /**
+     * @return array<string, array{'displayType': DisplayType}>
+     */
+    public static function initializesTotalsByDisplayTypeDataProvider(): array
+    {
+        return [
+            'including tax' => [
+                'displayType' => DisplayType::IncludingTax,
+            ],
+            'including and excluding tax' => [
+                'displayType' => DisplayType::Both,
             ],
         ];
     }
---
+++ test/Integration/_files/order_with_custom_fees_taxed.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+use JosephLeedy\CustomFees\Api\CustomOrderFeesRepositoryInterface;
+use JosephLeedy\CustomFees\Api\Data\CustomOrderFeeInterface;
+use JosephLeedy\CustomFees\Api\Data\CustomOrderFeesInterface;
+use JosephLeedy\CustomFees\Api\Data\CustomOrderFeesInterfaceFactory;
+use JosephLeedy\CustomFees\Model\CustomOrderFeesRepository;
+use JosephLeedy\CustomFees\Model\FeeType;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\ResourceModel\Order as OrderResource;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\Workaround\Override\Fixture\Resolver;
+
+Resolver::getInstance()->requireDataFixture('Magento/Sales/_files/order_with_tax.php');
+
+$objectManager = Bootstrap::getObjectManager();
+/** @var Order $order */
+$order = $objectManager->create(Order::class);
+/** @var OrderResource $orderResource */
+$orderResource = $objectManager->create(OrderResource::class);
+/** @var CustomOrderFeesInterfaceFactory $customOrderFeesFactory */
+$customOrderFeesFactory = $objectManager->create(CustomOrderFeesInterfaceFactory::class);
+/** @var CustomOrderFeesInterface $customOrderFees */
+$customOrderFees = $customOrderFeesFactory->create();
+/** @var CustomOrderFeesRepository $customOrderFeesRepository */
+$customOrderFeesRepository = $objectManager->create(CustomOrderFeesRepositoryInterface::class);
+$testCustomFees = [
+    'test_fee_0' => $objectManager->create(
+        CustomOrderFeeInterface::class,
+        [
+            'data' => [
+                'code' => 'test_fee_0',
+                'title' => 'Test Fee',
+                'type' => FeeType::Fixed,
+                'percent' => null,
+                'show_percentage' => false,
+                'base_value' => 5.00,
+                'value' => 5.00,
+                'base_value_with_tax' => 5.42,
+                'value_with_tax' => 5.42,
+                'base_tax_amount' => 0.42,
+                'tax_amount' => 0.42,
+            ],
+        ],
+    ),
+    'test_fee_1' => $objectManager->create(
+        CustomOrderFeeInterface::class,
+        [
+            'data' => [
+                'code' => 'test_fee_1',
+                'title' => 'Another Test Fee',
+                'type' => FeeType::Fixed,
+                'percent' => null,
+                'show_percentage' => false,
+                'base_value' => 1.50,
+                'value' => 1.50,
+                'base_value_with_tax' => 1.63,
+                'value_with_tax' => 1.63,
+                'base_tax_amount' => 0.13,
+                'tax_amount' => 0.13,
+            ],
+        ],
+    ),
+];
+
+$orderResource->load($order, '100000001', 'increment_id');
+
+/** @var int $orderId */
+$orderId = $order->getEntityId() ?? 0;
+
+$customOrderFees->setOrderId($orderId);
+$customOrderFees->setCustomFeesOrdered($testCustomFees);
+
+$customOrderFeesRepository->save($customOrderFees);
+
+$order
+    ->getExtensionAttributes()
+    ?->setCustomOrderFees($customOrderFees);
